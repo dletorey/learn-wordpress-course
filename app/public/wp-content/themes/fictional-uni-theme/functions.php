003d76ab017e41b8e01048e5732ebc2a915b75e2
Dave Letorey <dave@code-red.uk>
2021-03-25 14:42:28 +0000

    loaded external stylesheet

--- app/public/wp-content/themes/fictional-uni-theme/functions.php
+++ app/public/wp-content/themes/fictional-uni-theme/functions.php
@@ -1,5 +1,6 @@
 <?php 
 function university_files() {
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
 
