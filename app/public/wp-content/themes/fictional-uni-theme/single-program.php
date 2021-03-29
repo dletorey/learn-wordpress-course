55f99e0b18974db89d66f0267f153666db534229
Dave Letorey <dave@code-red.uk>
2021-03-29 10:47:04 +0100

    changed the subheading to Program

--- app/public/wp-content/themes/fictional-uni-theme/single-program.php
+++ app/public/wp-content/themes/fictional-uni-theme/single-program.php
@@ -6,7 +6,7 @@
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
-        <p>EVENT</p>
+        <p>Program</p>
       </div>
     </div>  
   </div>
