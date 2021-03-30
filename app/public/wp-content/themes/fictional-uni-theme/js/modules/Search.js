625276d44e286ee79e5d5a42d07a2919b8326d58
Dave Letorey <dave@code-red.uk>
2021-03-30 17:24:51 +0100

    added the key events to open and close the search overlay

--- app/public/wp-content/themes/fictional-uni-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-uni-theme/js/modules/Search.js
@@ -4,8 +4,9 @@ class SearchObject {
         this.openButton = document.querySelectorAll(".js-search-trigger");
         this.closeButton = document.querySelector(".search-overlay__close");
         this.searchOverlay = document.querySelector(".search-overlay");
+        this.body = document.querySelector("body");
         this.events();
- 
+        this.isOverlayOpen = false;
     }
  
     //2. events
@@ -13,16 +14,28 @@ class SearchObject {
         this.openButton[0].addEventListener('click', this.openOverlay.bind(this));
         this.openButton[1].addEventListener('click', this.openOverlay.bind(this));
         this.closeButton.addEventListener('click', this.closeOverlay.bind(this));
- 
+        document.addEventListener('keyup', this.keyPressDispatcher.bind(this));
     }
  
     //3. methods (or functions)
+    keyPressDispatcher(e) {
+        if (e.keyCode == 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+        if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
     openOverlay(){
         this.searchOverlay.classList.add('search-overlay--active');
+        this.body.classList.add('body-no-scroll');
+        this.isOverlayOpen = true;
     }
- 
+    
     closeOverlay(){
         this.searchOverlay.classList.remove('search-overlay--active');
+        this.body.classList.remove('body-no-scroll');
+        this.isOverlayOpen = false;
     }
 }
  
