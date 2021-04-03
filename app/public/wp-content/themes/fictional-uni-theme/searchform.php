5350d6f34cd8aa637d6bb3775badcca7f66277ea
Dave Letorey <dave@code-red.uk>
2021-04-03 15:12:51 +0100

    created search results page

---
+++ app/public/wp-content/themes/fictional-uni-theme/searchform.php
@@ -0,0 +1,7 @@
+<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+    <label for="s" class="headline headline--medium">Perform a new Search</label>
+    <div class="search-form-row">
+        <input class="s" type="search" name="s" id="s" placeholder="What are you looking for?" >
+        <input class="search-submit" type="submit" value="Search">
+    </div>
+</form>
