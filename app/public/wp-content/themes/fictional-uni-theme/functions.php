48fd7361e8480baf30d29f7f88ae415cf09dcd41
Dave Letorey <dave@code-red.uk>
2021-04-06 14:25:49 +0100

    checked to see if current user is a subscriber and redirected them to frontend with no admin bar

--- app/public/wp-content/themes/fictional-uni-theme/functions.php
+++ app/public/wp-content/themes/fictional-uni-theme/functions.php
@@ -99,5 +99,27 @@ function universityMapKey($api) {
     return $api;
 }
 
-add_filter('acf/fields/google_map/api', 'universityMapKey')
+add_filter('acf/fields/google_map/api', 'universityMapKey');
+
+// redirect subscriber accounts out of admin
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+    $ourCurrentUser = wp_get_current_user();
+
+    if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+        wp_redirect(site_url('/'));
+        exit;
+    }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+    $ourCurrentUser = wp_get_current_user();
+
+    if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+        show_admin_bar(false);
+    }
+}
 ?>
