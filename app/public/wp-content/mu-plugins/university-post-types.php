10c5b960c3028f7218fe7e4995c1eea5142e90a8
Dave Letorey <dave@code-red.uk>
2021-04-19 15:58:44 +0100

    created the note post type

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -49,9 +49,23 @@ function university_post_types() {
         ),
         'menu_icon' => 'dashicons-awards'
       ));
-    register_post_type('professor', array(
+    register_post_type('note', array(
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => false,
+        'show_ui' => true,
+        'labels' => array(
+          'name' => 'Notes',
+          'add_new_item' => 'Add New Note',
+          'edit_item' => 'Edit Note',
+          'all_items' => 'All Notes',
+          'singular_name' => 'Note'
+        ),
+        'menu_icon' => 'dashicons-welcome-write-blog'
+      ));
+    register_post_type('professor', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
         'public' => true,
         'labels' => array(
           'name' => 'Professors',
