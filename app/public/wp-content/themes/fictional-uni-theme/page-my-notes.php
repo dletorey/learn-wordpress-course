e347239f01c43f1fe2fef22e50bb2ff182bf2f9d
Dave Letorey <dave@code-red.uk>
2021-04-19 15:33:38 +0100

    update to 5.7.1 and created my-notes template

---
+++ app/public/wp-content/themes/fictional-uni-theme/page-my-notes.php
@@ -0,0 +1,16 @@
+<?php 
+  if (!is_user_logged_in()) {
+    wp_redirect(esc_url(site_url('/')));
+    exit;
+  }
+  get_header();
+    while(have_posts()) {
+        the_post(); 
+        pageBanner(); ?>
+  <div class="container container--narrow page-section">
+      Custom code here
+  </div>
+
+    <?php }
+    get_footer();
+?>
