2cd95963cfdc1691aa2c73249db1fc38b0c19e3a
Dave Letorey <dave@code-red.uk>
2021-04-20 15:58:40 +0100

    created methods to control likeing and unliking

--- app/public/wp-content/themes/fictional-uni-theme/js/modules/like.js
+++ app/public/wp-content/themes/fictional-uni-theme/js/modules/like.js
@@ -1,14 +1,28 @@
+import $ from 'jquery';
+
 class Like {
     constructor() {
-        
+        this.events();
     }
 
     events() {
-
+        $(".like-box").on("click", this.ourClickDispatcher.bind(this));
     }
 
     // methods
-
+    ourClickDispatcher() {
+        if($(".like-box").data('exists') == 'yes') {
+            this.deleteLike();
+        } else {
+            this.createLike();
+        }
+    }
+    createLike() {
+        alert("Create Like");
+    }
+    deleteLike() {
+        alert("Delete Like");
+    }
 }
 
 export default Like;
