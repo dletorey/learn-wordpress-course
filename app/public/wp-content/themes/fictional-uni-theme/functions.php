1cd5dec9542a9d80130de05d7fb3347d62146639
Dave Letorey <dave@code-red.uk>
2021-04-20 14:47:12 +0100

    added a rule to make sure there is a limit to the number of posts and make sure that it only works on creating

--- app/public/wp-content/themes/fictional-uni-theme/functions.php
+++ app/public/wp-content/themes/fictional-uni-theme/functions.php
@@ -145,10 +145,13 @@ function ourLoginTitle() {
 }
 
 // Force note posts to be private
-add_filter('wp_insert_post_data', 'makeNotePrivate');
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
 
-function makeNotePrivate($data) {
+function makeNotePrivate($data, $postarr) {
     if ($data['post_type'] == 'note') {
+        if(count_user_posts(get_current_user_id(), 'note') > 4 AND !$postarr['ID']) {
+            die("You have reached your note limit");
+        }
         $data['post_content'] = sanitize_textarea_field($data['post_content']);
     }
     if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
