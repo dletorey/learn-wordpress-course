2084188082e49111648095991871e99706df4f1a
Dave Letorey <dave@code-red.uk>
2021-04-22 12:23:31 +0100

    new day updates

--- app/public/wp-content/themes/fictional-uni-theme/functions.php
+++ app/public/wp-content/themes/fictional-uni-theme/functions.php
@@ -50,9 +50,9 @@ function university_files() {
     if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
         wp_enqueue_script('main-uni-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     } else {
-        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.00130cd66ffc72a49409.js'), NULL, '1.0', true);
-        wp_enqueue_script('main-uni-js', get_theme_file_uri('/bundled-assets/scripts.748a59170fb4fbb822ff.js'), NULL, '1.0', true);
-        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.748a59170fb4fbb822ff.css'));
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.07cb7586e2052fd66bd1.js'), NULL, '1.0', true);
+        wp_enqueue_script('main-uni-js', get_theme_file_uri('/bundled-assets/scripts.b0a0f04967105f232f21.js'), NULL, '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.b0a0f04967105f232f21.css'));
     }
     wp_localize_script('main-uni-js', 'uniData', array(
         'root_url' => get_site_url(),
@@ -139,7 +139,7 @@ add_action('login_enqueue_scripts', 'ourLoginCSS');
 
 function ourLoginCSS() {
     wp_enqueue_style('roboto_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.748a59170fb4fbb822ff.css'));
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.b0a0f04967105f232f21.css'));
 }
 
 add_filter ('login_headertitle', 'ourLoginTitle');
